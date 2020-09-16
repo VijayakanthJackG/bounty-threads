08cf7e377c3aae804ab8f45d9ea12075b8e48523
VijayakanthJackG <http://geekcreativeagency.com/>
2020-09-16 10:23:10 +0530

    first commit

---
+++ brand-threads/main_link_css.php
@@ -0,0 +1,64 @@
+<link rel="stylesheet" href="public/css/bootstrap.css">
+<link rel="stylesheet" href="public/css/font-awesome.min.css">
+<link rel="stylesheet" href="public/css/style.css">
+<link rel="stylesheet" href="public/css/animate.min.css">
+<link rel="stylesheet" href="public/css/responsive.css">
+<link rel="stylesheet" href="public/css/jquery-ui.css" />
+<link rel="stylesheet" href="public/css/owl.carousel.min.css" />
+<link rel="stylesheet" href="public/css/owl.theme.default.min.css" />
+
+<link rel="stylesheet" type="text/css" href="public/css/slick.css">
+  <link rel="stylesheet" type="text/css" href="public/css/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="public/css/ani.css">
+
+  <style type="text/css">
+   .spinner.fixed {
+    position: fixed;
+}
+.spinner {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1003;
+    background: #ebeff5;
+    overflow: hidden;
+} 
+
+
+#preloader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  /* change if the mask should have another color then white */
+  z-index: 99;
+  /* makes sure it stays on top */
+}
+
+#status {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  left: 50%;
+  /* centers the loading animation horizontally one the screen */
+  top: 50%;
+  /* centers the loading animation vertically one the screen */
+ /* background-image: url(https://raw.githubusercontent.com/niklausgerber/PreLoadMe/master/img/status.gif); */
+  /* path to your loading animation */
+  background-repeat: no-repeat;
+  background-position: center;
+  margin: -100px 0 0 -100px;
+  /* is width and height divided by two */
+}
+
+  </style>
+
+
+
+<!-- Include Below JS After Your jQuery.min File -->
+<script src="public/js/jquery-3.3.1.min.js"></script>
+
+
